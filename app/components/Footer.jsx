decdfe3556ddc70f29b7042118aa66ecd19b02cb
SarahCant <eaa23smtwc@students.eaaa.dk>
2024-12-03 15:28:32 +0100

    Start Footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,13 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <section className="footer bg-[color:--main] text-[color:--background] flex flex-row justify-between p-6">
-      <div className="footer-contact">
-        <h3>Kontakt</h3>
-      </div>
-      <div className="footer-some">
+    <footer className="footer bg-[color:--main] text-[color:--background] flex flex-col p-6 gap-8 items-center md:flex-row md:justify-between">
+      <section className="footer-contact flex flex-col justify-between gap-3 items-center">
+        <h3 className="text-center">Kontakt</h3>
+        <div className="flex flex-row gap-2">
+          <img src="/img/icons/mail.png" alt="E-mail" className="h-4" />
+          <p>birgitteeichild@hotmail.com</p>
+        </div>
+        <p className="flex flex-row gap-2">
+          <Image
+            src="/img/icons/phone.png"
+            width={20}
+            height={20}
+            alt="Telefon"
+          ></Image>
+          +45 40 95 29 65
+        </p>
+        <p className="flex flex-row gap-2">
+          <Image
+            src="/img/icons/pin.png"
+            width={20}
+            height={20}
+            alt="Adresse"
+          ></Image>
+          Bedervej 101 8320 Mårslet
+        </p>
+      </section>
+      <section className="footer-some">
         <h3>Følg med</h3>
-      </div>
+        <div className="flex flex-row gap-5">
+          <Link href="https://www.facebook.com/triyogaAarhus/">
+            <Image
+              src="/img/icons/fb.png"
+              width={20}
+              height={20}
+              alt="Facebook"
+            ></Image>
+          </Link>
+
+          <Link href="https://www.instagram.com/birgitte_eichild/">
+            <Image
+              src="/img/icons/ig.png"
+              width={20}
+              height={20}
+              alt="Instagram"
+            ></Image>
+          </Link>
+        </div>
+      </section>
       {/* logo */}
-    </section>
+    </footer>
   );
 }
