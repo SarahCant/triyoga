7cc18b18795de6403a2d605bcd662d42399ad1ad
SarahCant <eaa23smtwc@students.eaaa.dk>
2024-12-04 12:30:33 +0100

    Booking

--- app/booking/page.jsx
+++ app/booking/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default async function Booking() {
   const response = await fetch(
     "https://triyoga-bbaf1-default-rtdb.firebaseio.com/teams.json"
@@ -16,13 +18,70 @@ export default async function Booking() {
   //console.log(teams);
 
   return (
-    <main>
-      <h1>booking</h1>
-      {/*      <ol>
-        {teams.map((team) => {
-          return <li key={team.id}>{team.name}</li>;
-        })}
-      </ol> */}
+    <main className="px-4 md:pt-10">
+      <h1 className="text-center">Booking</h1>
+      {/* text section */}
+      <div className="border-x-8 border-[color:--main]">
+        {/* outer border */}
+        <div className="bg-[color:--orange] p-4 border-x-8 border-[color:#769975] ">
+          <p>
+            I kalenderen nedenfor, ser du en oversigt over de yogahold og
+            -workshops, som du kan tilmelde dig. For at tilmelde dig et bestemt
+            hold, skal du trykke på “Book” knappen på dette hold. Herefter skal
+            du oprette dig som bruger eller logge ind. Så bliver du ført til den
+            endelige bookingside. Du kan læse mere om de enkelte tilbud ved at
+            trykke på “Læs mere”.
+          </p>
+          {/* icon section */}
+          <section className="flex justify-evenly py-8 items-center">
+            <div className="flex flex-col justify-center items-center ">
+              <Image
+                src="/img/icons/calendar.png"
+                width={30}
+                height={30}
+                alt="Kalender"
+              />
+              <p>Vælg hold</p>
+            </div>
+            <Image
+              src="/img/icons/arrow-black.png"
+              width={30}
+              height={10}
+              alt="Pil til højre"
+            />
+
+            <div className="flex flex-col justify-center items-center text-center">
+              <Image
+                src="/img/icons/profile.png"
+                width={30}
+                height={30}
+                alt="Profil"
+              />
+              <p>
+                Log ind/
+                <br />
+                Opret bruger
+              </p>
+            </div>
+
+            <Image
+              src="/img/icons/arrow-black.png"
+              width={30}
+              height={10}
+              alt="Pil til højre"
+            />
+            <div className="flex flex-col justify-center items-center">
+              <Image
+                src="/img/icons/checkmark.png"
+                width={30}
+                height={30}
+                alt="Gennemført"
+              />
+              <p>Book</p>
+            </div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,61 +1,4 @@
-/* import Image from "next/image";
-import Link from "next/link";
-
-export default function Footer() {
-  return (
-    <footer className="footer bg-[color:--main] text-[color:--background] flex flex-col p-6 gap-8 items-center md:flex-row md:justify-between">
-      <section className="footer-contact flex flex-col justify-between gap-3 items-center">
-        <h3 className="text-center">Kontakt</h3>
-        <div className="flex flex-row gap-2">
-          <img src="/img/icons/mail.png" alt="E-mail" className="h-4" />
-          <p>birgitteeichild@hotmail.com</p>
-        </div>
-        <p className="flex flex-row gap-2">
-          <Image
-            src="/img/icons/phone.png"
-            width={20}
-            height={20}
-            alt="Telefon"
-          ></Image>
-          +45 40 95 29 65
-        </p>
-        <p className="flex flex-row gap-2">
-          <Image
-            src="/img/icons/pin.png"
-            width={20}
-            height={20}
-            alt="Adresse"
-          ></Image>
-          Bedervej 101 8320 Mårslet
-        </p>
-      </section>
-      <section className="footer-some">
-        <h3>Følg med</h3>
-        <div className="flex flex-row gap-5">
-          <Link href="https://www.facebook.com/triyogaAarhus/">
-            <Image
-              src="/img/icons/fb.png"
-              width={30}
-              height={30}
-              alt="Facebook"
-            ></Image>
-          </Link>
-
-          <Link href="https://www.instagram.com/birgitte_eichild/">
-            <Image
-              src="/img/icons/ig.png"
-              width={30}
-              height={30}
-              alt="Instagram"
-            ></Image>
-          </Link>
-        </div>
-      </section>
-      <Image src="/img/logo.png" width={30} height={30} alt="Logo"></Image>
-    </footer>
-  );
-}
- */ import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
@@ -118,7 +61,7 @@ export default function Footer() {
         </section>
       </div>
       {/* logo */}
-      <div className="flex justify-center mt-10 md:mt-5 md:opacity-70">
+      <div className="flex justify-center mt-10 md:mt-5 opacity-70">
         <Link href="/">
           <Image src="/img/logo.png" width={50} height={50} alt="Logo" />
         </Link>
