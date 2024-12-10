1482d50b170a964281bd87b699a0ac0ea85a4f8d
SarahCant <eaa23smtwc@students.eaaa.dk>
2024-12-10 17:47:34 +0100

    Add booked to user

--- app/auth/helpers.js
+++ app/auth/helpers.js
@@ -12,12 +12,17 @@ export async function getUserByMail(mail) {
 export async function createUser(user) {
   console.log("user create", user);
 
+  const userWithBooked = {
+    ...user,
+    booked: "",
+  };
+
   const url = `${process.env.NEXT_PUBLIC_FB_DB_URL}/users.json`;
   const response = await fetch(url, {
     method: "POST",
-    body: JSON.stringify(user),
+    body: JSON.stringify(userWithBooked),
   });
   const data = await response.json();
 
-  return { id: data.name, ...user };
+  return { id: data.name, ...userWithBooked };
 }
